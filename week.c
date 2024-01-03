bd604d009bb5d326c8f6efda6bb08896585d7568
Gaurang-dobariya <150230396+Gaurang-dobariya@users.noreply.github.com>
2024-01-03 22:55:45 +0530

    Add files via upload

---
+++ week.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+enum week{
+	
+	sun=11,mon,tue,wed=02,thu,fri,sat
+	
+};
+
+void main()
+{
+	
+	printf("Sun = %d\n",sun);
+	printf("Mon = %d\n",mon);
+	printf("Tue = %d\n",tue);
+	printf("Wed = %d\n",wed);
+	printf("Thu = %d\n",thu);
+	printf("Fri = %d\n",fri);
+	printf("Sat = %d\n",sat);
+	
+}
+
